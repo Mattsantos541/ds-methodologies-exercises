b79b9cab83367fac59546d4e8cc91c7c2acf439b
Matt Santos <mattsantos541@gmail.com>
2019-10-10 09:57:37 -0500

    split scale

---
+++ regression/split_scale.py
@@ -0,0 +1,2 @@
+###split_scale.py
+
--- regression/wrangle.py
+++ regression/wrangle.py
@@ -1,4 +1,10 @@
-print ("hello world")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
-import pandas as import pd
-pd.read_
+
+import env
